a7d27b47725e6a5758959f88d5cedf48bcd79ffa
mak7chek <mak77chek@gmail.com>
2025-03-24 02:05:05 +0200

    2. Отримання даних з JSON: Завантаження даних 3. Перетворення даних у масив: Обробка JSON даних та створення масиву об'єктів для подальшого використання. 4. Відображення даних на екрані: Відображення інформації про товари та категорії у вигляді стилізованих блоків за допомогою CSS.

---
+++ js/addProducts.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () =>{
+  fetch("data.json")
+    .then(response => response.json())//тіпа обєкт створили
+    .then(data => {
+      console.log(data.products);
+      console.log(data);
+      displayProducts(data.products);
+    })
+    .catch(error => console.log("Помилка з JSON:", error));
+});
+
+function displayProducts(products){
+  const gallery = document.querySelector(".product-gallery");
+  gallery.innerHTML = "";
+  products.forEach(product => {
+    const card=document.createElement('div');
+    card.classList.add("product-card");
+
+    card.innerHTML =`
+ <img src="${product.image}" alt = "${product.name}">
+<h3>${product.name}</h3>
+<p> Ціна: ${product.price} грн</p>
+<button class="btn">Додавти в кошик</button> `;
+    gallery.appendChild(card);
+  });
+}
+
